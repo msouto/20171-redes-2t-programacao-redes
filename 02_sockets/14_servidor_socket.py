9c3f6f825c9e6adb33c57756d05c128347197be5
Moisés Souto <moises.souto@gmail.com>
2017-05-15 21:45:44 -0300

    Estrutura do servidor para receber conexoes

--- 02_sockets/14_servidor_socket.py
+++ 02_sockets/14_servidor_socket.py
@@ -9,3 +9,17 @@ tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #associacao de porta ao processo servidor
 tcp_server_socket.bind((HOST,PORT))
 tcp_server_socket.listen()
+
+client, addr = tcp_server_socket.accept()
+While True:
+    #receber os dados do cliente
+    data = client.recv(1024)
+    #preparo mensagem de confirmacao de recebimento
+    message = "Recebido"
+    byte_msg = message.encode('utf-8')
+    #envio da mensagem de confirmacao
+    client.send(byte_msg)
+    if not data: break
+    print("\n Mensagem recebida:", data)
+client.close()
+tcp_server_socket.close()
