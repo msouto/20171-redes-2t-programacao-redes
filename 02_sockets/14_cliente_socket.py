37c402c9dd2f6970af6057d54cd52365b4853716
Moisés Souto <moises.souto@gmail.com>
2017-05-17 19:33:23 -0300

    Exemplo de cliente TCP.IP em python

---
+++ 02_sockets/14_cliente_socket.py
@@ -0,0 +1,17 @@
+import socket
+HOST = socket.gethostbyname('localhost')
+PORT = 2000
+
+tcp_client_socket = socket.socket(sock.AF_INET, socket.SOCK_STREAM)
+
+#Estrutura inicial do client TCP/IP
+tcp_client_socket.connect((HOST,PORT))
+
+#preparando menssagem
+message = "Hello World"
+byte_msg = message.encode('utf_8')
+
+tcp_client_socket.send(byte_msg)
+data = tcp_client_socket.recv(1024)
+tcp_client_socket.close
+print("MSG recebida:", data)
